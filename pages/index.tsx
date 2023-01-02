e3326ffd0e89237afd4f18823e046e42a2c19e36
quocna <anhquocngo1011@gmail.com>
2023-01-02 21:52:37 +0700

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,10 +40,10 @@ const Home: NextPageWithLayout = () => {
     if (loadElement.current && observer.current)
       observer.current.observe(loadElement.current);
 
-    return () => {
-      if (observer.current)
-        observer.current.disconnect();
-    };
+    // return () => {
+    //   if (observer.current)
+    //     observer.current.disconnect();
+    // };
   }, [loadElement, observer]);
 
   useEffect(() => {
